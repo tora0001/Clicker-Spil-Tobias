9e742b032ad78b2daeb191286066485279583509
tora0001 <tobe.2309@hotmail.com>
2023-02-21 12:13:45 +0100

    script added

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
+"use strict";
 console.log("script.js is running");
+
+window.addEventListener("load", start);
+
+function start() {
+  console.log("script.js is running");
+
+  document.querySelector("#mink_1_container").classList.add("mink_1_move");
+
+  document
+    .querySelector("#mink_1_container")
+    .addEventListener("mousedown", clickMink1);
+}
+
+function clickMink1() {
+  document
+    .querySelector("#mink_1_container")
+    .removeEventListener("mousedown", clickMink1);
+
+  document.querySelector("#mink_1_container").classList.add("paused");
+
+  document.querySelector("#mink_1_sprite").classList.add("disapear");
+
+  document
+    .querySelector("#mink_1_container")
+    .addEventListener("disapear", minkGone);
+}
+
+function minkGone() {
+  document
+    .querySelector("#mink_1_container")
+    .removeEventListener("animationend", minkGone);
+
+  document.querySelector("#mink_1_sprite").classList.remove("disapear");
+
+  document.querySelector("#mink_1_container").classList.remove("paused");
+}
